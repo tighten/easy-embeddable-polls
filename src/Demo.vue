66fee8dcbefbc722bd65ea4ddfb5f6c4e8275a48
John T. Bonaccorsi <jtbonaccorsi@gmail.com>
2018-09-17 15:41:59 -0400

    Make title shorter

--- src/Demo.vue
+++ src/Demo.vue
@@ -4,13 +4,13 @@
     :choices="['Skittles', 'Starburst', 'Nerds']"
     custom-answer-label="Other Candy"
     endpoint="https://jsonplaceholder.typicode.com/posts"
-    error-message="There was a problem submitting your favorite bread."
+    error-message="There was a problem submitting your favorite candy."
     :multiple-choice="false"
     :request-config="{ 'X-Requested-With': 'XMLHttpRequest' }"
     :submit-error-hook="onError"
     :submit-success-hook="onSuccess"
     thank-you-message="Thanks for your opinion on rainbow candy!"
-    title="What's your favorite rainbow colored candy?"
+    title="What's your favorite rainbow candy?"
   >
   </poll>
 </template>
